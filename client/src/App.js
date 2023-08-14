7bcc7bdd615d0ca5b62193802968242ae2a79a9a
arnavbahadur <bahadurarnav2203@gmail.com>
2023-08-15 00:47:45 +0530

    folder with footer

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+// import Navbar from "./Universal/Navbar";
+import Footer from "./Universal/Footer";
+function App() {
+  return (
+    <div className="App">
+     {/* <Navbar/> */}
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
